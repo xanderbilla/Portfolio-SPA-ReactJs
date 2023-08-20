8fd5667d442d5d6ce0301ec246bf882cfee678a1
Vikas Singh <xanderbilla@gmail.com>
2023-08-20 09:17:21 +0300

    #Logo Context - Bug Fixed

--- src/assets/Brand.js
+++ src/assets/Brand.js
@@ -1,12 +1,14 @@
 import { useContext } from "react";
+import { ThemeContext } from '../Context'
 
 function Brand(props) {
 
     const theme = useContext(ThemeContext);
+    console.log(theme.state?.darkMode);
 
     return (
         <img
-            src={theme.state.darkmode ? 'https://i.imgur.com/S5awLwi.png' : 'https://i.imgur.com/G6sWWqH.png'}
+            src={theme.state.darkMode ? 'https://i.imgur.com/S5awLwi.png' : 'https://i.imgur.com/G6sWWqH.png'}
             width={props.width}
             height={props.height}
             className="d-inline-fixed align-top"
