33bbdc90cd0d26145ee70ecaf95994b01cbb62d4
ＶＩＫＡＳ | ᓕᘏᗢ <33988028+xanderbilla@users.noreply.github.com>
2023-08-20 05:58:32 +0000

    #Logo Updated

--- src/assets/Brand.js
+++ src/assets/Brand.js
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+
 function Brand(props) {
+
+    const theme = useContext(ThemeContext);
+
     return (
         <img
-            src="https://i.imgur.com/lJ7QosV.png"
+            src={theme.state.darkmode ? 'https://i.imgur.com/S5awLwi.png' : 'https://i.imgur.com/G6sWWqH.png'}
             width={props.width}
             height={props.height}
             className="d-inline-fixed align-top"
